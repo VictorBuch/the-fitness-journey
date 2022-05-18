633b138fbb4b9e5ffc1d77b4625e781b146ca1e9
Victor Buch <47211196+VictorBuch@users.noreply.github.com>
2022-05-18 20:21:14 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    minWidth: {
+      '40': '10rem',
+      '60': '15rem',
+      '80': '20rem',
+      '100': '25rem',
+    },
+    maxWidth: {
+      '120': '30rem',
+      '160': '40rem',
+      '200': '50rem',
+    }
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      'dark'
+    ],
+  }
+}
